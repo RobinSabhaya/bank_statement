f1bec2b7f95e7919fe1424ee524c63f07f59c180
Robin Sabhaya <robin47ppsv@gmail.com>
2024-08-24 11:40:18 +0530

    Fix the issue of netlify redirect

--- src/components/helpers/api.helper.js
+++ src/components/helpers/api.helper.js
@@ -1,10 +1,13 @@
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
+
+console.log(REACT_APP_API_URL);
 const apiRoutes = {
   LOGIN: "/login",
   REGISTER: "/register",
   NEW_PASSWORD: "/new-password",
   RESET_PASSWORD: "/reset-password",
   QUERY_FORM: "/query-form",
-  CONVERT: "/api/v1/bsc/convert",
+  CONVERT: `${REACT_APP_API_URL}/api/v1/bsc/convert`,
   FILE_UPLOAD_EXCEL: "/api/v1/bsc/convert-to-excel",
   FILE_UPLOAD_EXCEL_ALL: "/api/v1/bsc/convert-to-excel-all",
   FILE_UPLOAD_CSV: "/api/v1/bsc/convert-to-csv",
