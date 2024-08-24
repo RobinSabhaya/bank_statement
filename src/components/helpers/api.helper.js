38be5122cbb43f21dd6beb8e52e0ec674ab7c245
Robin Sabhaya <robin47ppsv@gmail.com>
2024-08-24 12:47:17 +0530

    Add netlify.toml file

--- src/components/helpers/api.helper.js
+++ src/components/helpers/api.helper.js
@@ -1,12 +1,10 @@
-const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
-
 const apiRoutes = {
   LOGIN: "/login",
   REGISTER: "/register",
   NEW_PASSWORD: "/new-password",
   RESET_PASSWORD: "/reset-password",
   QUERY_FORM: "/query-form",
-  CONVERT: `${REACT_APP_API_URL}/api/v1/bsc/convert`,
+  CONVERT: `/api/v1/bsc/convert`,
   FILE_UPLOAD_EXCEL: "/api/v1/bsc/convert-to-excel",
   FILE_UPLOAD_EXCEL_ALL: "/api/v1/bsc/convert-to-excel-all",
   FILE_UPLOAD_CSV: "/api/v1/bsc/convert-to-csv",
